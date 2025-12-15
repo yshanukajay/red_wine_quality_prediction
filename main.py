066944665f547b287653a3542a3596eb724a4d56
JADY Shanuka <2021ICTS28@vauitc.com>
2025-12-15 14:30:42 +0530

    data ingestion completed

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from redwine import logger
+from redwine.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to out custom logging")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
